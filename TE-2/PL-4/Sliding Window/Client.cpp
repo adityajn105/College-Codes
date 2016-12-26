7feac40df3e822f66a38ab002f6d0462e7aa426c
aditya <adityajn105@gmail.com>
2016-12-26 22:31:00 +0530

    client-server c++ altered

--- TE-2/PL-4/Sliding Window/Client.cpp
+++ TE-2/PL-4/Sliding Window/Client.cpp
@@ -3,26 +3,51 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
-
 using namespace std;
 int main() {
     int clientSocket;
+    char data[1],ack[1],ldata;
     char buffer[1024];
+    
     struct sockaddr_in serverAddr;
     socklen_t addr_size;
-
     clientSocket=socket(AF_INET,SOCK_STREAM,0);     //create client socket
 
     serverAddr.sin_family=AF_INET;                  //address family
-    serverAddr.sin_port=htons(7951);                //port in network
+    serverAddr.sin_port=htons(7968);                //port in network
     serverAddr.sin_addr.s_addr=inet_addr("127.0.1.1");  //internet address
     memset(serverAddr.sin_zero, '\0',sizeof serverAddr.sin_zero);   //overwrite sinzero with zeroes
-
     addr_size=sizeof serverAddr;
     connect(clientSocket,(struct sockaddr *)&serverAddr,addr_size); //connect to server
-    recv(clientSocket,buffer,1024,0);       //receive data in buffer
-
-    cout<<"Packet No received is"<<buffer;
-
+    int curr=0;
+    ldata ='\0';
+    int lindex=-1;
+    while(curr<=10){
+        recv(clientSocket,data,1,0);
+        if(data[0]=='0' && ldata!='0' && lindex==-1){
+            ack[0]='0'+curr;
+            ldata='0';
+            send(clientSocket,ack,1,0);
+            cout<<"Packet Lost"<<endl;
+            lindex=curr;
+        }
+        else if(ldata=='0' && data[0]=='0' && lindex!=-1 ){
+            curr=lindex;
+            ack[0]='0'+(curr+1);
+            ldata='\0';
+            buffer[curr]='0';
+            cout << "Packet Recieved : Seq  " << curr+1 << " | Data : 0"<<endl;
+            send(clientSocket,ack,1,0);
+            lindex=-1;
+            curr++;
+        }
+        else if(lindex==-1){
+            ack[0]='0'+(curr+1);
+            buffer[curr]=data[0];
+            cout << "Packet Recieved : Seq  " << curr+1 << " | Data : " << data[0]<<endl;
+            send(clientSocket,ack,1,0);
+            curr++;
+        }
+    }
     return 0;
 }
--- TE-2/PL-4/Sliding Window/Server.cpp
+++ TE-2/PL-4/Sliding Window/Server.cpp
@@ -3,25 +3,25 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <string.h>
 
 using namespace std;
 
 
 int main() {
-    int serverSocket, clientSocket;                 //two socket for server and client
-    char buffer[1024];                              //data will be sent in buffer
+    int serverSocket, clientSocket,WINDOW=4;                 //two socket for server and client
+    char buffer[12];   //data will be sent in buffer
+    char ack[1],data[1];
     struct sockaddr_in serverAddr,clientAddr;      //structure representing server
     socklen_t addr_size;                            //address size of client
 
     serverSocket = socket(AF_INET,SOCK_STREAM,0); //domain,type,protocol
 
-    //AF_INET - ip4 internet protocols
+    //AF_INET - ip4 Internet protocols
     //SOCK_STREAM - Provides sequenced, reliable, two-way, connection-based byte streams.
     //Default(0) - TCP in this case
 
     serverAddr.sin_family=AF_INET;  //sa_family_t
-    serverAddr.sin_port =htons(7951); //in_port_t   //port to use for communications
+    serverAddr.sin_port =htons(7968); //in_port_t   //port to use for communications
     inet_aton("127.0.1.1", &serverAddr.sin_addr); //in_addr
     memset(serverAddr.sin_zero,'\0',sizeof serverAddr.sin_zero);   //memset- fill memory with constant byte
                                                                     //here fill serverAddr.sinzero with zeroes
@@ -34,9 +34,27 @@ int main() {
                                                                                     // and bind its address to clientAddr
     char *ip = inet_ntoa(clientAddr.sin_addr);
     cout<<"Connected to client@"<<ip<<endl;
-    cout<<"Packet no to be send :";
-    cin>>buffer;
-    send(clientSocket,buffer,1024,0);       //send buffer to client(client,buffer,size,0)
-    cout<<buffer<<" Sent";
+    strcpy(buffer,"baca059d0bc");
+    int len =strlen(buffer);
+    int curr=0;int acurr=0;
+    while(acurr<=len-1 || curr<=len-1) {
+        if (acurr < len) {
+            data[0] = buffer[acurr];
+            send(clientSocket, data, 1, 0);
+            cout << "Packet Sent : Seq - " << acurr+1 << " |  Data - " << data[0] << endl;
+            acurr++;
+        }
+        if(acurr-curr==WINDOW || acurr>=len-4){
+            recv(clientSocket,ack,1,0);
+            cout<<"Ack- "<<ack[0]<<endl;
+            int ak=ack[0]-'0';
+            if(curr==ak){
+                acurr=curr;
+            }
+            if(curr!=ak){
+                curr=ak;
+            }
+        }
+    }
     return 0;
 }
